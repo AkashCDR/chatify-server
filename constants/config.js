3ed71e639296132379c6749bcbe5d3aab1ae0d96
akash <akashshaw852@gmail.com>
2024-07-01 17:27:50 +0530

    url change

--- constants/config.js
+++ constants/config.js
@@ -1,8 +1,12 @@
 const corsOptions={
-      origin: "*",
+      origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
-      optionSuccessStatus:200,
+      
 }
 
 export const CHATIFY_TOKEN="chatify"
