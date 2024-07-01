a912da867ae6d512d1fa72ee900aafbf70e8cf7f
akash <akashshaw852@gmail.com>
2024-07-01 16:14:00 +0530

    third commit

--- constants/config.js
+++ constants/config.js
@@ -1,14 +1,24 @@
 const corsOptions={
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:4173",
-        "http://localhost:3000",
-        process.env.CLIENT_URL,
-      ],
+      origin: '*',
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
 }
 
-export const CHATIFY_TOKEN="chatify"
+// export const CHATIFY_TOKEN="chatify"
 
-export {corsOptions}
+// export {corsOptions}
+
+// const corsOptions={
+//   origin: [
+//       "http://localhost:5173",
+//       "http://localhost:4173",
+//       "http://localhost:3000",
+//       process.env.CLIENT_URL,
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+// }
+
+// export const CHATIFY_TOKEN="chatify"
+
+// export {corsOptions}
