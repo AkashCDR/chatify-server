a241661bde8c0ba856e61e43e47fd8b4069122a9
akash <akashshaw852@gmail.com>
2024-07-01 17:43:04 +0530

    app updated

--- app.js
+++ app.js
@@ -76,6 +76,15 @@ io.use((socket, next) => {
   );
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://chatify-client-rust.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+
 
 io.on("connection",(socket)=>{
     console.log("user connected",socket.id)
